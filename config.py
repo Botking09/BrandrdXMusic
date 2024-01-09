87a5b174ce090f0fe4dc9023c0d9a05855c1c212
Botking09 <156010397+Botking09@users.noreply.github.com>
2024-01-09 09:29:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,10 +83,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/ThanosXMuisc-start-pic-01-08"
+    "START_IMG_URL", "https://telegra.ph/Thanos-01-08-8"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/4f59fb748e1990acfa297.jpg"
+    "PING_IMG_URL", "https://telegra.ph/Thanos-01-08-8"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/4310ea5f523520b2b765b.jpg"
